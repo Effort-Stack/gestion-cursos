e8b80a36d2f110c8281f87af22012aeed25b133e
Josué Hernández <joheca1998@outlook.com>
2023-10-21 13:38:14 -0600

    Agregando creacion de beans de sesion a partir de entidades

---
+++ PRN315_GRUPO_02-ejb/src/java/proyecto/ejb/EstudianteFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.ejb;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import proyecto.entidades.Estudiante;
+
+/**
+ *
+ * @author johec
+ */
+@Stateless
+public class EstudianteFacade extends AbstractFacade<Estudiante> implements EstudianteFacadeLocal {
+
+    @PersistenceContext(unitName = "PRN315_GRUPO_02-ejbPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public EstudianteFacade() {
+        super(Estudiante.class);
+    }
+    
+}
