09ddd27c99fc6e1ce5f9a84a12a4c0e4294ff81f
Josué Hernández <joheca1998@outlook.com>
2023-10-21 13:48:43 -0600

    Agregando uso del archivo facadeLocal

--- PRN315_GRUPO_02-ejb/src/java/proyecto/ejb/EstudianteFacade.java
+++ PRN315_GRUPO_02-ejb/src/java/proyecto/ejb/EstudianteFacade.java
@@ -5,9 +5,11 @@
  */
 package proyecto.ejb;
 
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import proyecto.entidades.Estudiante;
 
 /**
@@ -27,6 +29,19 @@ public class EstudianteFacade extends AbstractFacade<Estudiante> implements Estu
 
     public EstudianteFacade() {
         super(Estudiante.class);
+    }
+
+    @Override
+    public List<Estudiante> buscarTodosAlfabeticamente() {
+        try {
+            em.getEntityManagerFactory().getCache().evictAll();
+            String consulta = "SELECT * FROM [Estudiante] order by nombre";
+            Query query = em.createNativeQuery(consulta, Estudiante.class);
+            List<Estudiante> listaResultado = query.getResultList();
+            return listaResultado;
+        } catch (Exception e) {
+            return null;
+        }
     }
     
 }
