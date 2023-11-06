fb29668ea6fb8c7df3c3af97e3eb37b9e93a7218
Josué Hernández <joheca1998@outlook.com>
2023-11-05 23:21:05 -0600

    Terminando formulario de profesores

--- PRN315_GRUPO_02-ejb/src/java/proyecto/ejb/ProfesorFacade.java
+++ PRN315_GRUPO_02-ejb/src/java/proyecto/ejb/ProfesorFacade.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyecto.ejb;
 
 import java.util.List;
@@ -12,10 +7,6 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import proyecto.entidades.Profesor;
 
-/**
- *
- * @author johec
- */
 @Stateless
 public class ProfesorFacade extends AbstractFacade<Profesor> implements ProfesorFacadeLocal {
 
--- PRN315_GRUPO_02-war/src/java/proyecto/forms/ProfesorForm.java
+++ PRN315_GRUPO_02-war/src/java/proyecto/forms/ProfesorForm.java
@@ -3,7 +3,12 @@ package proyecto.forms;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import proyecto.ejb.ProfesorFacadeLocal;
 import proyecto.entidades.Profesor;
 
 @Named(value = "profesorForm")
@@ -11,18 +16,18 @@ import proyecto.entidades.Profesor;
 public class ProfesorForm implements Serializable {
 
     private Profesor profesor;
+    private Profesor selectedProfesor; // Añadido para manejar la selección en la tabla
+
+    @EJB
+    private ProfesorFacadeLocal profesorFacade;
 
     @PostConstruct
     public void init() {
-        try {
-            // Código de inicialización del formulario
-            profesor = new Profesor();
-        } catch (Exception e) {
-            // Manejar la excepción adecuadamente
-        }
+        profesor = new Profesor();
     }
 
     public ProfesorForm() {
+        // Constructor vacío necesario para JPA
     }
 
     public Profesor getProfesor() {
@@ -31,5 +36,49 @@ public class ProfesorForm implements Serializable {
 
     public void setProfesor(Profesor profesor) {
         this.profesor = profesor;
+    }
+    
+    public Profesor getSelectedProfesor() {
+        return selectedProfesor;
+    }
+
+    public void setSelectedProfesor(Profesor selectedProfesor) {
+        this.selectedProfesor = selectedProfesor;
+    }
+
+    public void guardarProfesor() {
+        try {
+            profesorFacade.create(profesor);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito", "Profesor agregado correctamente."));
+            profesor = new Profesor(); // Reiniciar el objeto profesor para limpiar el formulario
+        } catch (Exception e) {
+            e.printStackTrace(); // Imprime la traza de la excepción para depuración
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "No se pudo agregar el profesor."));
+        }
+    }
+
+    
+    public void actualizarProfesor() {
+        try {
+            profesorFacade.edit(selectedProfesor); // Actualizar el profesor seleccionado
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito", "Profesor actualizado correctamente."));
+            selectedProfesor = null; // Reiniciar la selección
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "No se pudo actualizar el profesor."));
+        }
+    }
+
+    public void eliminarProfesor() {
+        try {
+            profesorFacade.remove(selectedProfesor); // Eliminar el profesor seleccionado
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito", "Profesor eliminado correctamente."));
+            selectedProfesor = null; // Reiniciar la selección
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "No se pudo eliminar el profesor."));
+        }
+    }
+
+    public List<Profesor> getListaProfesores() {
+        return profesorFacade.findAll();
     }
 }
