a1671dc015a1707c1a72321c56e8aa9bedc9a8a1
Josué Hernández <joheca1998@outlook.com>
2023-11-06 00:01:28 -0600

    Terminando formulario de Curso

--- PRN315_GRUPO_02-ejb/src/java/proyecto/entidades/Curso.java
+++ PRN315_GRUPO_02-ejb/src/java/proyecto/entidades/Curso.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyecto.entidades;
 
 import java.io.Serializable;
@@ -23,10 +18,6 @@ import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
-/**
- *
- * @author johec
- */
 @Entity
 @Table(name = "Curso")
 @XmlRootElement
--- PRN315_GRUPO_02-war/src/java/proyecto/forms/CursoForm.java
+++ PRN315_GRUPO_02-war/src/java/proyecto/forms/CursoForm.java
@@ -3,7 +3,12 @@ package proyecto.forms;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import proyecto.ejb.CursoFacadeLocal;
 import proyecto.entidades.Curso;
 
 @Named(value = "cursoForm")
@@ -12,17 +17,17 @@ public class CursoForm implements Serializable {
 
     private Curso curso;
 
+    // Inyectar la fachada local de Cursos usando EJB
+    @EJB
+    private CursoFacadeLocal cursoFacade;
+
     @PostConstruct
     public void init() {
-        try {
-            // Código de inicialización del formulario
-            curso = new Curso();
-        } catch (Exception e) {
-            // Manejar la excepción adecuadamente
-        }
+        curso = new Curso();
     }
 
     public CursoForm() {
+        // Constructor vacío necesario para JPA
     }
 
     public Curso getCurso() {
@@ -31,5 +36,43 @@ public class CursoForm implements Serializable {
 
     public void setCurso(Curso curso) {
         this.curso = curso;
+    }
+
+    // Método para guardar un curso en la base de datos
+    public void guardarCurso() {
+        try {
+            cursoFacade.create(curso);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito", "Curso agregado correctamente."));
+            curso = new Curso(); // Reiniciar el objeto para limpiar el formulario
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "No se pudo agregar el curso."));
+        }
+    }
+
+    // Método para actualizar un curso existente en la base de datos
+    public void actualizarCurso() {
+        try {
+            cursoFacade.edit(curso);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito", "Curso actualizado correctamente."));
+            curso = new Curso(); // Opcionalmente reiniciar el objeto
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "No se pudo actualizar el curso."));
+        }
+    }
+
+    // Método para eliminar un curso de la base de datos
+    public void eliminarCurso() {
+        try {
+            cursoFacade.remove(curso);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito", "Curso eliminado correctamente."));
+            curso = new Curso(); // Reiniciar el objeto para limpiar el formulario
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "No se pudo eliminar el curso."));
+        }
+    }
+
+    // Método para obtener una lista de todos los cursos
+    public List<Curso> getListaCursos() {
+        return cursoFacade.findAll();
     }
 }
